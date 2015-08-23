8b8aff4c2513b9ea73af480f936afe3a5c16e728
Rachel Evans <git@rve.org.uk>
2015-08-23 23:11:30 +0100

    ec2 collector

--- aws-query.js
+++ aws-query.js
@@ -1,3 +1,4 @@
+var EC2Collector = require('./ec2-collector');
 var IAMCollector = require('./iam-collector');
 var SQSCollector = require('./sqs-collector');
 var Q = require('q');
@@ -5,6 +6,7 @@ var Q = require('q');
 Q.longStackSupport = true;
 
 Q.all([
+    EC2Collector.collectAll(),
     IAMCollector.collectAll(),
     SQSCollector.collectAll()
 ]).done();
---
+++ ec2-collector.js
@@ -0,0 +1,41 @@
+var AWS = require('aws-sdk');
+var Q = require('q');
+
+var AwsDataUtils = require('./aws-data-utils');
+
+var promiseEC2 = function () {
+    return Q(new AWS.EC2({ region: 'eu-west-1' }));
+};
+
+var describeInstances = function (ec2) {
+    return AwsDataUtils.collectFromAws(ec2, "ec2", "describeInstances", [])
+        .then(function (r) {
+            r.Reservations.sort(function (a, b) {
+                if (a.ReservationId < b.ReservationId) return -1;
+                else if (a.ReservationId > b.ReservationId) return +1;
+                else return 0;
+            });
+            for (var i=0; i<r.Reservations.length; ++i) {
+                r.Reservations[i].Instances.sort(function (a, b) {
+                    if (a.InstanceId < b.InstanceId) return -1;
+                    else if (a.InstanceId > b.InstanceId) return +1;
+                    else return 0;
+                });
+            }
+            return r;
+        });
+};
+
+var collectAll = function () {
+    var ec2 = promiseEC2();
+
+    var di = ec2.then(describeInstances).then(AwsDataUtils.saveJsonTo("var/ec2/describe-instances.json"));
+
+    return Q.all([
+        di
+    ]);
+};
+
+module.exports = {
+    collectAll: collectAll
+};
