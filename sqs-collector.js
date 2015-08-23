ef2c7abf99985be4b0ef67305b7b07c06e9ee0b7
Rachel Evans <git@rve.org.uk>
2015-08-23 20:54:52 +0100

    sqs/list-all-queues.txt

--- sqs-collector.js
+++ sqs-collector.js
@@ -1,5 +1,6 @@
 var AWS = require('aws-sdk');
 var Q = require('q');
+var path = require('path');
 
 var AwsDataUtils = require('./aws-data-utils');
 var SqsListAllQueues = require('./sqs-list-all-queues');
@@ -12,10 +13,19 @@ var listAllQueues = function (sqs) {
     return SqsListAllQueues.listAllQueues(sqs);
 };
 
+var queueUrlsToNames = function(r) {
+    var s = '';
+    for (var i=0; i < r.QueueUrls.length; ++i) {
+        s += path.basename(r.QueueUrls[i]) + "\n";
+    }
+    return s;
+};
+
 var collectAll = function () {
     var sqs = promiseSQS();
 
-    return sqs.then(listAllQueues).then(AwsDataUtils.saveJsonTo("var/sqs/list-all-queues.json"));
+    return sqs.then(listAllQueues).then(AwsDataUtils.saveJsonTo("var/sqs/list-all-queues.json"))
+        .then(queueUrlsToNames).then(AwsDataUtils.saveContentTo("var/sqs/list-all-queues.txt"));
 };
 
 module.exports = {
